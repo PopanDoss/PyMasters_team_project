12a72cb7cd78cad74000d9ae9971d6e977929168
PopanDoss <chernenko.seroja@gmail.com>
2024-07-29 23:25:56 +0300

    Final_ver.2

--- pymasters/services/email.py
+++ pymasters/services/email.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+
 from fastapi_mail import FastMail, MessageSchema, MessageType
 from fastapi_mail.errors import ConnectionErrors
 from pydantic import EmailStr
 
-from pymasters.repository.users_repo import UserService
-from pymasters.services.auth_service import create_email_token
-from pymasters.settings import conf
+from repository.users_repo import UserService
+from services.auth_service import create_email_token
+from settings import conf
 
 async def send_email(email: EmailStr, host: str):
     """
@@ -22,24 +23,16 @@ async def send_email(email: EmailStr, host: str):
     - ConnectionErrors: If there is a problem connecting to the email server or sending the email.
     """
     try:
-        # Generate a verification token for the email address
         token_verification = create_email_token({"sub": email})
-
-        # Create the email message schema
         message = MessageSchema(
-            subject="Confirm your email",
+            subject="Confirm your email ",
             recipients=[email],
             template_body={"host": host, "token": token_verification},
             subtype=MessageType.html
         )
 
-        # Initialize FastMail with configuration settings
         fm = FastMail(conf)
-
-        # Send the email using the specified template
-        await fm.send_message(message, template_name="email_template.html")
-
+        await fm.send_message(message, template_name="email_tamplate.html")
     except ConnectionErrors as err:
-        # Print the error if email sending fails
-        print(f"Error sending email: {err}")
+        print(err)
 
